885ab1d556c601082de3f48465de842e013fee6c
21120553 <tantran.300803@gmail.com>
2023-10-28 02:20:11 +0700

    update

--- js/app.js
+++ js/app.js
@@ -6,19 +6,3 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
-
-var swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-        delay: 500000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    loop: true,
-});
-
-console.log(swiper);

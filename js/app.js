659f3f8c584114fb84d6cda1f80f1707d5a43341
21120553 <tantran.300803@gmail.com>
2023-11-05 01:20:31 +0700

    update

--- js/app.js
+++ js/app.js
@@ -6,3 +6,19 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+// Lấy tất cả các thẻ <a> trong menu
+const navLinks = document.querySelectorAll(".navbar a");
+
+// Lặp qua từng thẻ <a> và thêm sự kiện click
+navLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+        // Loại bỏ lớp "active" từ tất cả các thẻ <a>
+        navLinks.forEach((navLink) => {
+            navLink.classList.remove("active");
+        });
+
+        // Thêm lớp "active" cho thẻ <a> được click
+        link.classList.add("active");
+    });
+});
